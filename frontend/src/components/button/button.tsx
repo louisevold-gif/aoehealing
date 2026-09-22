9057f93d50031874997ff403f3bffb2c7f9ff24b
HenrikFalla <henrik.falla@gmail.com>
2026-09-22 16:39:16 +0200

    Button component

---
+++ frontend/src/components/button/button.tsx
@@ -0,0 +1,26 @@
+import styles from './button.module.css'
+
+interface ButtonProps {
+  children: React.ReactNode
+  onClick?: () => void
+  type?: 'button' | 'submit' | 'reset'
+  disabled?: boolean
+}
+
+export default function Button({
+  children,
+  onClick,
+  type,
+  disabled,
+}: ButtonProps) {
+  return (
+    <button
+      className={styles.button}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  )
+}
